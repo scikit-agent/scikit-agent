25b22e69afd855882102e54abd5ac59f6b9b7caa
sb <sbenthall@gmail.com>
2025-03-19 12:07:19 -0400

    formatting test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,10 +2,7 @@ import skagent.utils as utils
 
 
 def test_utils_apply_fun_to_vals():
-    fun = lambda x,y: x ** y
-    vals = {
-        'x' : 2,
-        'y' : 3
-    }
+    fun = lambda x, y: x**y
+    vals = {"x": 2, "y": 3}
     a = utils.apply_fun_to_vals(fun, vals)
-    assert(a == 8)
+    assert a == 8
