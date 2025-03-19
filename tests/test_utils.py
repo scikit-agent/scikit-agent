cdbc037073d7a4c52be2bc200708987dc5601a7d
sb <sbenthall@gmail.com>
2025-03-19 13:14:57 -0400

    mypy update

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,9 @@ import skagent.utils as utils
 
 
 def test_utils_apply_fun_to_vals():
-    fun = lambda x, y: x**y
+    def pow(x, y):
+        return x**y
+
     vals = {"x": 2, "y": 3}
-    a = utils.apply_fun_to_vals(fun, vals)
+    a = utils.apply_fun_to_vals(pow, vals)
     assert a == 8
