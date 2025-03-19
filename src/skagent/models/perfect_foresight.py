bdd9aa0c8b9bc8b81591a9091dee6d99daceca90
sb <sbenthall@gmail.com>
2025-03-19 16:37:32 -0400

    bringing in generic model tools from (S)HARK. fixes, with tests. Fixes #4

---
+++ src/skagent/models/perfect_foresight.py
@@ -0,0 +1,28 @@
+from HARK.distributions import Bernoulli
+from HARK.model import Control, DBlock
+
+calibration = {
+    "DiscFac": 0.96,
+    "CRRA": 2.0,
+    "Rfree": 1.03,
+    "LivPrb": 0.98,
+    "PermGroFac": 1.01,
+    "BoroCnstArt": None,
+}
+
+block = DBlock(
+    **{
+        "name": "consumption",
+        "shocks": {
+            "live": Bernoulli(p=calibration["LivPrb"]),
+        },
+        "dynamics": {
+            "y": lambda p: p,
+            "m": lambda Rfree, a, y: Rfree * a + y,
+            "c": Control(["m"], upper_bound=lambda m: m),
+            "p": lambda PermGroFac, p: PermGroFac * p,
+            "a": lambda m, c: m - c,
+        },
+        "reward": {"u": lambda c, CRRA: c ** (1 - CRRA) / (1 - CRRA)},
+    }
+)
